27ec4bda1eb7971f52d1ecc960eec8a29876548e
jacksoncc <jacksonh_pzo@hotmail.com>
2014-04-26 01:52:00 -0300

    Testando leitura do arquivo pelo scanner

--- LinguagemJL.java
+++ LinguagemJL.java
@@ -1,3 +1,7 @@
+import java.util.Scanner;
+import java.io.File;
+
+
 class LinguagemJL{
 	public static void main(String[] args){
 		// Início do projeto do interpretador de linguagem em java
@@ -10,16 +14,20 @@ class LinguagemJL{
 		// criando o leitor
 		Scanner leitor;
 		
-        Interpretador interpretador;
+        // criando o interpretador
+        //Interpretador interpretador;
         String linhas[] = new String[2000]; // arquivo pode ter, no máximo, 2000 linhas.
         
         try {
             // Referencia o arquivo. args[0] conterá os dados passados pela linha de comando (arquivo com codigo a ser interpretado).
             arquivo = new File(args[0]);
+            System.out.println("arquivo criado com sucesso");
+            
             // Mandamos o Scanner ler a partir do arquivo.
             leitor = new Scanner(arquivo);
+            System.out.println("Arquivo lido com sucesso");
             // Instanciamos o interpretador.
-            interpretador = new Interpretador();
+			//interpretador = new Interpretador();
             
             // Lemos todas as linhas do arquivo para dentro do
             // vetor "linhas".
@@ -32,11 +40,11 @@ class LinguagemJL{
             // Inicializamos o interpretador com o vetor de linhas. A partir
             // desse ponto, o objeto "b" irá interpretar o código lido do arquivo.
             //b.interpreta(linhas);
-            
+			
         } catch (Exception e) {
             System.out.println("Nao consegui ler o arquivo: " + (args.length > 0 ? args[0] : "(desconhecido)"));
             System.out.println("Uso:");
-            System.out.println("    java Blah /caminho/para/arquivo.blah");
+            System.out.println("    java LinguagemJL Main.JL");
         }
 		
 		
